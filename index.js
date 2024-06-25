5389176dea91929440ee8f9ccc65d82b73d31174
peyton brimmer <peytonbrimmer0@gmail.com>
2024-06-25 19:39:10 -0400

    optimized sub optimal code

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ function createLogo(response) {
     console.log(response);
     const svg = shape(response);
     fs.writeFile(name, svg, () => console.log('Logo Generated'));
+<<<<<<< HEAD
 }
+=======
+    }
+>>>>>>> 240be7fe28583155fe9acb3b72e5516ee3515540
 
     function initialize() {
         inquirer
